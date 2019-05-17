009f1918bf15ee01cfcc66c642aa5b4ee886c88f
Tristan Viney <yenivyeniv@gmail.com>
2019-05-17 12:58:17 +0200

    centered design, added padding to heading

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,8 +2,8 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
-    strings: ["Look at all these crazy cocktails", "Look at them!", "Make them!!!", "No seriously...", "Don't make me come down there"],
-    typeSpeed: 50,
+    strings: ["Look at all these crazy cocktails", "Look at them!", "Make them!!!", "No seriously...", "They're pretty damn tasty", "Don't beleive me?", "Fine", "Don't make me come down there"],
+    typeSpeed: 65,
     loop: true
   });
 }
